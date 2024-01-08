0b96950d62e92b894e8646f97c85a590cdc77f68
Alexander <vsany@meta.ua>
2024-01-08 17:01:05 +0200

    [+] UserCodec: EncodeUser

---
+++ FlutterApp.Server.Client.Codec/UserCodec.cs
@@ -0,0 +1,17 @@
+using FlutterApp.Server.Database.User;
+
+namespace FlutterApp.Server.Client.Codec;
+
+public static class UserCodec
+{
+    public static Payload.User.User EncodeUser(UserModel dbModel)
+    {
+        return new Payload.User.User()
+        {
+            Id = dbModel.Id,
+            FirstName = dbModel.FirstName,
+            LastName = dbModel.LastName,
+            HasPremium = dbModel.HasPremium
+        };
+    }
+}
