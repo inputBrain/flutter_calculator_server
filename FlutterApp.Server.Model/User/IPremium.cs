f2895e01300d69b887beb02252b7adcec5fb1e6c
Alexander <vsany@meta.ua>
2024-01-08 17:00:24 +0200

    in work: IPremium, IUser interfaces

---
+++ FlutterApp.Server.Model/User/IPremium.cs
@@ -0,0 +1,14 @@
+// using System;
+//
+// namespace FlutterApp.Server.Model.User;
+//
+// public interface IPremium
+// {
+//     int Id { get; }
+//     
+//     IUser UserModel { get; }
+//     
+//     DateTime? PremiumStartedAt { get; }
+//
+//     DateTime? PremiumEndedAt { get; }
+// }
---
+++ FlutterApp.Server.Model/User/IUser.cs
@@ -0,0 +1,18 @@
+// using System;
+//
+// namespace FlutterApp.Server.Model.User;
+//
+// public interface IUser
+// {
+//     int Id { get; }
+//     
+//     string FirstName { get; }
+//     
+//     string LastName { get; }
+//     
+//     bool HasPremium { get; }
+//     
+//     IPremium? Premium { get; }
+//     
+//     DateTime CreatedAt { get; }
+// }
