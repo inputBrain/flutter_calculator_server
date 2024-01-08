c5aff9ebbf9dc5600f00f2a8387c343048f37593
Alexander <vsany@meta.ua>
2024-01-08 17:44:12 +0200

    [+] UserController methods: GetOneUser, CreateUser, CreatePremium

--- FlutterApp.Server.Host/Controllers/Client/UserController.cs
+++ FlutterApp.Server.Host/Controllers/Client/UserController.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Threading.Tasks;
+using FlutterApp.Server.Client.Codec;
+using FlutterApp.Server.Client.Payload.User;
+using FlutterApp.Server.Client.User;
 using FlutterApp.Server.Database;
+using FlutterApp.Server.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,5 +15,54 @@ public class UserController : AbstractClientController<UserController>
     public UserController(ILogger<UserController> logger, IDatabaseContainer db) : base(logger, db)
     {
     }
-    
+
+
+    [HttpGet]
+    public async Task<GetOne.Response> GetOneUser([FromBody] GetOne request)
+    {
+        var model = await Db.UserRepository.GetOne(request.UserId);
+
+        return new GetOne.Response
+        {
+            Ussr = UserCodec.EncodeUser(model)
+        };
+    }
+
+
+    [HttpPost]
+    public async Task<CreateUser.Response> CreateUser([FromBody] CreateUser request)
+    {
+        var model = await Db.UserRepository.Create(request.FirstName, request.LastName);
+
+        return new CreateUser.Response
+        {
+            User = UserCodec.EncodeUser(model)
+        };
+    }
+
+
+    [HttpPost]
+    public async Task<CreatePremium.Response> CreatePremium([FromBody] CreatePremium request)
+    {
+        var user = await Db.UserRepository.GetOne(request.UserId);
+        
+        var premium = await Db.PremiumRepository.Create(
+            user,
+            request.StartedAt != null ? Timestamp.TimestampToDateTime(request.StartedAt.Value) : (DateTime?) null,
+            request.EndedAt != null ? Timestamp.TimestampToDateTime(request.EndedAt.Value) : (DateTime?) null
+        );
+
+        var updateHasPremiumStatus = await Db.UserRepository.UpdateHasPremium(user, true);
+
+        return new CreatePremium.Response()
+        {
+            Premium = new Premium()
+            {
+                Id = premium.Id,
+                User = UserCodec.EncodeUser(user),
+                StartedAt = premium.PremiumStartedAt != null ? Timestamp.ToUnixTime(premium.PremiumStartedAt.Value) : (int?) null,
+                EndedAt = premium.PremiumEndedAt != null ? Timestamp.ToUnixTime(premium.PremiumEndedAt.Value) : (int?) null,
+            }
+        };
+    }
 }
