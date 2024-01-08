4fcc18f59b9e10011c1e5f7197f6521cfb701eb3
Alexander <vsany@meta.ua>
2024-01-08 17:00:47 +0200

    in work: PremiumCodec

---
+++ FlutterApp.Server.Client.Codec/PremiumCodec.cs
@@ -0,0 +1,18 @@
+using FlutterApp.Server.Client.Payload.User;
+using FlutterApp.Server.Database.User.Premium;
+
+namespace FlutterApp.Server.Client.Codec;
+
+public static class PremiumCodec
+{
+    // public static Payload.User.Premium EncodePremium(PremiumModel dbModel)
+    // {
+    //     return new Premium()
+    //     {
+    //         Id = dbModel.Id,
+    //         User = UserCodec.EncodeUser(dbModel.User),
+    //         StartedAt =  dbModel.PremiumStartedAt,
+    //         EndedAt = dbModel.PremiumEndedAt
+    //     }
+    // }
+}
