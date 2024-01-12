f203486944808a2cf73754286ca3de74227fea5d
Alexander <vsany@meta.ua>
2024-01-12 11:10:46 +0200

    [+] Firebase DI

--- FlutterApp.Server.Host/Startup.cs
+++ FlutterApp.Server.Host/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using AxLink.Service;
 using FlutterApp.Server.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -6,18 +8,24 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace FlutterApp.Server.Host;
 
 public class Startup
 {
-    public Startup(IConfiguration configuration)
+    public IConfiguration Configuration { get; }
+    
+    private readonly ILoggerFactory _loggerFactory;
+    
+    
+    public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
     {
         Configuration = configuration;
+        _loggerFactory = loggerFactory;
     }
-
-    public IConfiguration Configuration { get; }
+    
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
@@ -28,6 +36,14 @@ public class Startup
                 Configuration.GetConnectionString("PostgreSqlConnection"),
                 b => b.MigrationsAssembly(typeOfContent.Assembly.GetName().Name)
             )
+        );
+
+        var firebaseConfig = Configuration.GetSection("Firebase").Get<Configs.Firebase>();
+
+        var firebaseService = new FirebaseService
+        (
+            _loggerFactory.CreateLogger<FirebaseService>(),
+            Directory.GetCurrentDirectory() + firebaseConfig?.AdminConfigPath
         );
 
         services.AddScoped<IDatabaseContainer, DatabaseContainer>();
