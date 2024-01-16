3da8d1fc78d4ffa34bf5bbf90e44ed7bc8874855
Alexander <vsany@meta.ua>
2024-01-16 11:14:59 +0200

    [+] init custom schema for controller's responses

--- FlutterApp.Server.Host/Startup.cs
+++ FlutterApp.Server.Host/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.IO;
 using AxLink.Service;
+using FlutterApp.Server.Client;
 using FlutterApp.Server.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -45,11 +47,38 @@ public class Startup
             _loggerFactory.CreateLogger<FirebaseService>(),
             Directory.GetCurrentDirectory() + firebaseConfig?.AdminConfigPath
         );
+        
+        string GetSchemaId(Type t)
+        {
+            var messagesRootNamespace = typeof(AbstractResponse).Namespace;
+            var messagesStartPrefix = messagesRootNamespace + '.';
+            
+            var fullname = t.FullName;
+
+            if (fullname.StartsWith(messagesStartPrefix) == false)
+            {
+                var value = typeof(IFormFile).Namespace;
+                if (value != null && fullname.StartsWith(value) == false)
+                {
+                    throw new Exception(
+                        $"Messages must be in {messagesRootNamespace} namespace, but this message is not: {fullname}"
+                    );
+                }
+            }
+
+            var prepared = fullname.Substring(messagesStartPrefix.Length).Replace('+', 'R');
+            return prepared;
+        }
 
         services.AddScoped<IDatabaseContainer, DatabaseContainer>();
 
         services.AddControllers();
-        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "FlutterApp.Host", Version = "v1"}); });
+        services.AddSwaggerGen(
+            c =>
+            {
+                c.CustomSchemaIds(GetSchemaId);
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "FlutterApp.Host", Version = "v1"});
+            });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
