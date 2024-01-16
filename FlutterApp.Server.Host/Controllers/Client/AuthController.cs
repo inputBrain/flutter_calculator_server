8fe4712588c31129a20598d0830cb1dda7f8e0a9
Alexander <vsany@meta.ua>
2024-01-16 11:18:05 +0200

    [+] added method LoginByFirebase in AuthController

--- FlutterApp.Server.Host/Controllers/Client/AuthController.cs
+++ FlutterApp.Server.Host/Controllers/Client/AuthController.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using AxLink.Service;
 using FlutterApp.Server.Client.Auth;
 using FlutterApp.Server.Database;
+using FlutterApp.Server.Database.SocialIdentity;
+using FlutterApp.Server.Model.SocialIdentity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -35,6 +37,23 @@ public class AuthController : AbstractClientController<AuthController>
         {
             throw new Exception("Invalid Firebase token");
         }
-        var user = await Db.UserRepository.
+
+        var socialUserModel = new SocialIdentityModel();
+        var user = await Db.SocialIdentityRepository.FindByUid(socialUser.Uid);
+        if (user.Item1 == null && user.Item2 == null)
+        {
+            var userModel = await Db.UserRepository.Create(socialUser.FirstName, socialUser.LastName, socialUser.PhoneNumber, DateTime.Now);
+            
+            
+            socialUserModel = await Db.SocialIdentityRepository.Create(
+                userModel.Id, 
+                socialUser.Uid,
+                user.Item1.SocialUid,
+                SocialType.Google,
+                DateTime.Now
+                );
+        }
+
+        return SendOk(socialUserModel);
     }
 }
